34a8e02e44acac6ba5987d0018c8ba5459c7b2f5
karoolina1234 <karoolina.sm@gmail.com>
2024-07-16 13:47:25 -0300

    Criando a Ação e multação

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,10 @@
 </template>
 
 <script>
-import axios from "axios";
 
 export default {
   created() {
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      console.log(response.data);
-    });
+   this.$store.dispatch('loadProducts')
   },
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+  },
+  actions: {
+    loadProducts() {
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        this.commit("loadProducts", response.data);
+      });
+    },
+  },
   modules: {},
 });
